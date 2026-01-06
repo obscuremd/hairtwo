924ee8a6aed9fe821db0f560f24a9fd0353a5dc5
dan <md.erhenede@gmail.com>
2026-01-06 11:22:42 +0100

    cc

--- hairixifyfour/app/product/[id]/page.tsx
+++ hairixifyfour/app/product/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import Blog from "@/screens/ServiceProviderScreen/Blog";
 import Hero from "@/screens/ServiceProviderScreen/Hero";
 import Reviews from "@/screens/ServiceProviderScreen/Reviews";
 import Services from "@/screens/ServiceProviderScreen/Services";
@@ -11,15 +12,16 @@ export default function index() {
   return (
     <div className="">
       <Hero />
-      <div className="space-y-2 p-5 md:p-[68px] bg-[#F3FAF4]">
+      {/* <div className="space-y-2 p-5 md:p-[68px] bg-[#F3FAF4]">
         <p className="text-xl md:text-[2rem] font-bold">About Us</p>
         <p className="md:text-lg font-medium">
           For any kind of make up - Owanbe, video/photoshoot, traditional,
           registry/court - We got your covered.
         </p>
-      </div>
+      </div> */}
       <Services />
       <Reviews />
+      <Blog />
     </div>
   );
 }
